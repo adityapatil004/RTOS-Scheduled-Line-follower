9cddd053337c82eb6ff58d1de594f09b1c480245
wunderkin <107317442+wunderkin@users.noreply.github.com>
2023-07-22 19:43:39 +0530

    Add files via upload

---
+++ Inc/DIO.h
@@ -0,0 +1,31 @@
+
+#ifndef DIO_H_
+#define DIO_H_
+
+#define HIGH 1
+#define LOW  0
+#define INPUT 0
+#define OUTPUT 1
+#define ALTERNATE_FUNCTION 2
+#define ANALOG 3
+#define GPIOAEN 0
+#define GPIOBEN 1
+#define GPIOCEN 2
+#define GPIODEN 3
+#define GPIOEEN 4
+#define GPIOHEN 7
+#define NO_PUPD 0
+#define PULL_UP 1
+#define PULL_DOWN 2
+#define RESERVED 3
+
+
+
+int GPIO_digital_read(GPIO_TypeDef* GPIOx, int Pin_No);
+void GPIO_digital_write(GPIO_TypeDef* GPIOx,int Pin_No, int Pin_Status);
+void GPIO_pin_mode(GPIO_TypeDef* GPIOx,int Pin_no, int mode);
+void GPIO_clock_enable(GPIO_TypeDef* GPIOx);
+void GPIO_Alternate_func_select(GPIO_TypeDef* GPIOx,int Pin_No,int AFR_Type);
+void GPIO_pin_PUPD(GPIO_TypeDef* GPIOx,int Pin_no, int PUPD_mode);
+
+#endif /*DIO_H_ */
---
+++ Inc/Motor_driver.h
@@ -0,0 +1,20 @@
+/*
+ * Motor_drive.h
+ *
+ *  Created on: May 28, 2023
+ *      Author: Aditya
+ */
+
+#ifndef MOTOR_DRIVER_H_
+#define MOTOR_DRIVER_H_
+#include "DIO.h"
+
+void Motor_Driver_Init();
+void LEFT_MOTOR_DRIVE(int dir, int DC);
+void RIGHT_MOTOR_DRIVE(int dir, int DC);
+
+#define FORWARD 1
+#define BACKWARD 0
+#define STOP -1
+
+#endif /* MOTOR_DRIVER_H_ */
